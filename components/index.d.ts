cb3b8da9178696a352c47cbfae243b835e8f0781
LAPTOP-3TEHD5AV\Work <gretavait@gmail.com>
2023-06-29 12:17:03 +0200

    flex component added

--- components/index.d.ts
+++ components/index.d.ts
@@ -5,4 +5,31 @@ interface ButtonBase {
 }
 declare const Button: ({ children }: ButtonBase) => react_jsx_runtime.JSX.Element;
 
-export { Button };
+interface FlexProps {
+    children: React.ReactNode;
+    display?: "flex" | "inline-flex";
+    alignItems?: "flex-start" | "center" | "flex-end" | "baseline" | "stretch";
+    justifyContent?: "flex-start" | "center" | "flex-end" | "space-between" | "stretch";
+    flexDirection?: "row" | "column" | "column-reverse";
+    wrap?: boolean;
+    columnGap?: {
+        sm?: number;
+        lg?: number;
+    };
+    rowGap?: {
+        sm?: number;
+        lg?: number;
+    };
+    fullHeight?: boolean;
+    fullWidth?: boolean;
+    className?: string;
+    spacing?: {
+        sm?: number;
+        lg?: number;
+    };
+    center?: boolean;
+    onClick?: Function;
+}
+declare const Flex: ({ children, display, alignItems, justifyContent, flexDirection, wrap, columnGap, rowGap, fullHeight, fullWidth, className, spacing, center, onClick, }: FlexProps) => react_jsx_runtime.JSX.Element;
+
+export { Button, Flex };
--- components/index.js
+++ components/index.js
@@ -20,7 +20,8 @@ var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: tru
 // src/components/index.ts
 var components_exports = {};
 __export(components_exports, {
-  Button: () => Button_default
+  Button: () => Button_default,
+  Flex: () => Flex_default
 });
 module.exports = __toCommonJS(components_exports);
 
@@ -30,7 +31,104 @@ var Button = ({ children }) => {
   return /* @__PURE__ */ (0, import_jsx_runtime.jsx)("button", { className: "button", children });
 };
 var Button_default = Button;
+
+// src/functions/useMediaQuery.ts
+var import_react = require("react");
+function useMediaQuery(query) {
+  const [matches, setMatches] = (0, import_react.useState)(false);
+  (0, import_react.useEffect)(() => {
+    const media = window.matchMedia(`(min-width: ${query}px)`);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+    const listener = () => {
+      setMatches(media.matches);
+    };
+    media.addEventListener("change", listener);
+    return () => media.removeEventListener("change", listener);
+  }, [matches, query]);
+  return matches;
+}
+
+// src/functions/getStyles.ts
+var getStyle = (value, isDesktop) => {
+  if (isDesktop) {
+    if (value.lg == 0) {
+      return "0";
+    }
+    if (value.lg) {
+      return convertUnit(value.lg);
+    }
+    if (value.sm) {
+      return convertUnit(value.sm);
+    }
+  } else {
+    if (value.sm) {
+      return convertUnit(value.sm);
+    }
+    return "0";
+  }
+};
+var convertUnit = (value) => {
+  return `${value / 10}rem`;
+};
+
+// src/components/Flex.tsx
+var import_jsx_runtime2 = require("react/jsx-runtime");
+var Flex = ({
+  children,
+  display = "flex",
+  alignItems,
+  justifyContent,
+  flexDirection = "row",
+  wrap,
+  columnGap,
+  rowGap,
+  fullHeight,
+  fullWidth,
+  className = "",
+  spacing,
+  center,
+  onClick
+}) => {
+  const isDesktop = useMediaQuery(1168);
+  const customStyles = {};
+  if (columnGap) {
+    customStyles.columnGap = getStyle(columnGap, isDesktop);
+  }
+  if (rowGap) {
+    customStyles.rowGap = getStyle(rowGap, isDesktop);
+  }
+  if (spacing) {
+    customStyles.marginBottom = getStyle(spacing, isDesktop);
+  }
+  return /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(
+    "div",
+    {
+      className: `
+        flex-8us4I&
+        flex-8us4I&-${display}
+        flex-8us4I&-${`alignItems-${alignItems}`}
+        flex-8us4I&-${`justifyContent-${justifyContent}`}
+        flex-8us4I&-${`flexDirection-${flexDirection}`}
+        ${wrap ? "flex-8us4I&-wrap" : ""}
+        ${fullHeight ? "flex-8us4I&-fullHeight" : ""}
+        ${fullWidth ? "flex-8us4I&-fullWidth" : ""}
+        ${center ? "flex-8us4I&-center" : ""}
+        ${className}
+      `,
+      style: customStyles,
+      onClick: () => {
+        onClick && onClick();
+      },
+      role: onClick ? "button" : "",
+      children
+    }
+  );
+};
+var Flex_default = Flex;
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
-  Button
+  Button,
+  Flex
 });
