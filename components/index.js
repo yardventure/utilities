6570486ae9b2013451649004843dedce409c36df
LAPTOP-3TEHD5AV\Work <gretavait@gmail.com>
2023-06-30 15:48:54 +0200

    minify output

--- components/index.js
+++ components/index.js
@@ -1,393 +1,44 @@
-"use strict";
-var __defProp = Object.defineProperty;
-var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-var __getOwnPropNames = Object.getOwnPropertyNames;
-var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __export = (target, all) => {
-  for (var name in all)
-    __defProp(target, name, { get: all[name], enumerable: true });
-};
-var __copyProps = (to, from, except, desc) => {
-  if (from && typeof from === "object" || typeof from === "function") {
-    for (let key of __getOwnPropNames(from))
-      if (!__hasOwnProp.call(to, key) && key !== except)
-        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
-  }
-  return to;
-};
-var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
-
-// src/components/index.ts
-var components_exports = {};
-__export(components_exports, {
-  Box: () => Box_default,
-  Button: () => Button_default,
-  Flex: () => Flex_default,
-  Grid: () => Grid_default,
-  Text: () => Text_default
-});
-module.exports = __toCommonJS(components_exports);
-
-// src/custom/LoadingSpinner.tsx
-var import_jsx_runtime = require("react/jsx-runtime");
-var LoadingSpinner = () => {
-  return /* @__PURE__ */ (0, import_jsx_runtime.jsx)(
-    "div",
-    {
-      className: `
+"use strict";var S=Object.defineProperty;var A=Object.getOwnPropertyDescriptor;var J=Object.getOwnPropertyNames;var U=Object.prototype.hasOwnProperty;var X=(t,e)=>{for(var r in e)S(t,r,{get:e[r],enumerable:!0})},H=(t,e,r,n)=>{if(e&&typeof e=="object"||typeof e=="function")for(let s of J(e))!U.call(t,s)&&s!==r&&S(t,s,{get:()=>e[s],enumerable:!(n=A(e,s))||n.enumerable});return t};var z=t=>H(S({},"__esModule",{value:!0}),t);var tt={};X(tt,{Box:()=>F,Button:()=>v,Flex:()=>P,Grid:()=>W,Text:()=>G});module.exports=z(tt);var R=require("react/jsx-runtime"),K=()=>(0,R.jsx)("div",{className:`
         loader-8us4IE
-      `
-    }
-  );
-};
-var LoadingSpinner_default = LoadingSpinner;
-
-// src/functions/getStyles.ts
-var getStyleArray = (value, isDesktop) => {
-  if (isDesktop) {
-    if (value.lg) {
-      return value.lg.map((val) => convertUnit(val)).join(" ");
-    }
-    if (value.sm) {
-      return value.sm.map((val) => convertUnit(val)).join(" ");
-    }
-  } else {
-    if (value.sm) {
-      return value.sm.map((val) => convertUnit(val)).join(" ");
-    }
-    return "0";
-  }
-};
-var getStyle = (value, isDesktop) => {
-  if (isDesktop) {
-    if (value.lg == 0) {
-      return "0";
-    }
-    if (value.lg) {
-      return convertUnit(value.lg);
-    }
-    if (value.sm) {
-      return convertUnit(value.sm);
-    }
-  } else {
-    if (value.sm) {
-      return convertUnit(value.sm);
-    }
-    return "0";
-  }
-};
-var getStyleSpacing = (value, isDesktop) => {
-  if (isDesktop) {
-    if (value.lg == 0) {
-      return value.lg;
-    }
-    return value.lg || value.sm;
-  } else {
-    if (value.sm) {
-      return value.sm;
-    }
-    return "0";
-  }
-};
-var convertUnit = (value) => {
-  return `${value / 10}rem`;
-};
-
-// src/components/Button.tsx
-var import_jsx_runtime2 = require("react/jsx-runtime");
-var Button = ({
-  as,
-  buttonColor,
-  textColor,
-  href,
-  external,
-  variant = "",
-  buttonHeight = 56,
-  buttonWidth,
-  borderRadius = 8,
-  shadow = false,
-  border,
-  loading = false,
-  disabled = false,
-  startIcon,
-  endIcon,
-  className = "",
-  hint,
-  transform,
-  children,
-  onClick
-}) => {
-  const handleClick = () => {
-    if (!disabled && onClick) {
-      onClick();
-      return;
-    }
-  };
-  const customStyles = {};
-  if (buttonWidth) {
-    customStyles.width = convertUnit(buttonWidth);
-  }
-  if (buttonHeight) {
-    customStyles.height = convertUnit(buttonHeight);
-  }
-  if (borderRadius) {
-    customStyles.borderRadius = convertUnit(borderRadius);
-  }
-  if (href && onClick) {
-    throw new Error("Please provider either href or onClick");
-  }
-  return /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(
-    "button",
-    {
-      style: customStyles,
-      className: `
+      `}),k=K;var w=(t,e)=>{if(e){if(t.lg)return t.lg.map(r=>m(r)).join(" ");if(t.sm)return t.sm.map(r=>m(r)).join(" ")}else return t.sm?t.sm.map(r=>m(r)).join(" "):"0"},c=(t,e)=>{if(e){if(t.lg==0)return"0";if(t.lg)return m(t.lg);if(t.sm)return m(t.sm)}else return t.sm?m(t.sm):"0"},N=(t,e)=>e?t.lg==0?t.lg:t.lg||t.sm:t.sm?t.sm:"0",m=t=>`${t/10}rem`;var x=require("react/jsx-runtime"),O=({as:t,buttonColor:e,textColor:r,href:n,external:s,variant:o="",buttonHeight:i=56,buttonWidth:u,borderRadius:d=8,shadow:p=!1,border:h,loading:l=!1,disabled:f=!1,startIcon:a,endIcon:b,className:y="",hint:C,transform:B,children:Q,onClick:I})=>{let _=()=>{if(!f&&I){I();return}},$={};if(u&&($.width=m(u)),i&&($.height=m(i)),d&&($.borderRadius=m(d)),n&&I)throw new Error("Please provider either href or onClick");return(0,x.jsx)("button",{style:$,className:`
         button-8us4IE
-        button-8us4IE-buttonColor-${buttonColor}
-        button-8us4IE-textColor-${textColor}
-        ${variant ? `button-8us4IE-variant-${variant}` : ""}
-        ${border ? "button-8us4IE-border" : ""}
-        ${shadow ? "button-8us4IE-shadow" : ""}
-        ${loading ? "button-8us4IE-loading" : ""}
-        ${transform ? `button-8us4IE-transform-${transform}` : ""}
-        ${className}
-      `,
-      disabled: loading || disabled,
-      onClick: handleClick,
-      children: /* @__PURE__ */ (0, import_jsx_runtime2.jsxs)(LinkWrapper, { as, href, target: external ? "_blank" : "_self", children: [
-        startIcon,
-        children,
-        hint && /* @__PURE__ */ (0, import_jsx_runtime2.jsx)("span", { className: "button-8us4IE__hint", children: hint }),
-        endIcon,
-        loading && /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(LoadingSpinner_default, {})
-      ] })
-    }
-  );
-};
-var LinkWrapper = ({
-  as,
-  href,
-  target,
-  children
-}) => {
-  const Tag = as;
-  if (as && !href || !as && href) {
-    throw new Error("Please provide both - as and href props");
-  }
-  if (href) {
-    return /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(Tag, { href, target, className: "button-8us4IE-link", children });
-  }
-  return /* @__PURE__ */ (0, import_jsx_runtime2.jsx)("div", { className: "button-8us4IE-link", children });
-};
-var Button_default = Button;
-
-// src/functions/useMediaQuery.ts
-var import_react = require("react");
-function useMediaQuery(query) {
-  const [matches, setMatches] = (0, import_react.useState)(false);
-  (0, import_react.useEffect)(() => {
-    const media = window.matchMedia(`(min-width: ${query}px)`);
-    if (media.matches !== matches) {
-      setMatches(media.matches);
-    }
-    const listener = () => {
-      setMatches(media.matches);
-    };
-    media.addEventListener("change", listener);
-    return () => media.removeEventListener("change", listener);
-  }, [matches, query]);
-  return matches;
-}
-
-// src/components/Flex.tsx
-var import_jsx_runtime3 = require("react/jsx-runtime");
-var Flex = ({
-  children,
-  display = "flex",
-  alignItems,
-  justifyContent,
-  flexDirection = "row",
-  wrap,
-  columnGap,
-  rowGap,
-  fullHeight,
-  fullWidth,
-  className = "",
-  spacing,
-  center,
-  onClick
-}) => {
-  const isDesktop = useMediaQuery(1168);
-  const customStyles = {};
-  if (columnGap) {
-    customStyles.columnGap = getStyle(columnGap, isDesktop);
-  }
-  if (rowGap) {
-    customStyles.rowGap = getStyle(rowGap, isDesktop);
-  }
-  if (spacing) {
-    customStyles.marginBottom = getStyle(spacing, isDesktop);
-  }
-  return /* @__PURE__ */ (0, import_jsx_runtime3.jsx)(
-    "div",
-    {
-      className: `
+        button-8us4IE-buttonColor-${e}
+        button-8us4IE-textColor-${r}
+        ${o?`button-8us4IE-variant-${o}`:""}
+        ${h?"button-8us4IE-border":""}
+        ${p?"button-8us4IE-shadow":""}
+        ${l?"button-8us4IE-loading":""}
+        ${B?`button-8us4IE-transform-${B}`:""}
+        ${y}
+      `,disabled:l||f,onClick:_,children:(0,x.jsxs)(V,{as:t,href:n,target:s?"_blank":"_self",children:[a,Q,C&&(0,x.jsx)("span",{className:"button-8us4IE__hint",children:C}),b,l&&(0,x.jsx)(k,{})]})})},V=({as:t,href:e,target:r,children:n})=>{let s=t;if(t&&!e||!t&&e)throw new Error("Please provide both - as and href props");return e?(0,x.jsx)(s,{href:e,target:r,className:"button-8us4IE-link",children:n}):(0,x.jsx)("div",{className:"button-8us4IE-link",children:n})},v=O;var E=require("react");function g(t){let[e,r]=(0,E.useState)(!1);return(0,E.useEffect)(()=>{let n=window.matchMedia(`(min-width: ${t}px)`);n.matches!==e&&r(n.matches);let s=()=>{r(n.matches)};return n.addEventListener("change",s),()=>n.removeEventListener("change",s)},[e,t]),e}var D=require("react/jsx-runtime"),Y=({children:t,display:e="flex",alignItems:r,justifyContent:n,flexDirection:s="row",wrap:o,columnGap:i,rowGap:u,fullHeight:d,fullWidth:p,className:h="",spacing:l,center:f,onClick:a})=>{let b=g(1168),y={};return i&&(y.columnGap=c(i,b)),u&&(y.rowGap=c(u,b)),l&&(y.marginBottom=c(l,b)),(0,D.jsx)("div",{className:`
         flex-8us4IE
-        flex-8us4IE-${display}
-        flex-8us4IE-${`alignItems-${alignItems}`}
-        flex-8us4IE-${`justifyContent-${justifyContent}`}
-        flex-8us4IE-${`flexDirection-${flexDirection}`}
-        ${wrap ? "flex-8us4IE-wrap" : ""}
-        ${fullHeight ? "flex-8us4IE-fullHeight" : ""}
-        ${fullWidth ? "flex-8us4IE-fullWidth" : ""}
-        ${center ? "flex-8us4IE-center" : ""}
-        ${className}
-      `,
-      style: customStyles,
-      onClick: () => {
-        onClick && onClick();
-      },
-      role: onClick ? "button" : "",
-      children
-    }
-  );
-};
-var Flex_default = Flex;
-
-// src/components/Box.tsx
-var import_jsx_runtime4 = require("react/jsx-runtime");
-var Box = ({
-  children,
-  backgroundColor = "",
-  padding,
-  borderRadius,
-  borderWidth,
-  border = "",
-  opacity = 100,
-  shadow,
-  overflow = "visible",
-  className = "",
-  spacing,
-  onClick
-}) => {
-  const isDesktop = useMediaQuery(1168);
-  const customStyles = {
-    overflow
-  };
-  if (padding) {
-    customStyles.padding = getStyleArray(padding, isDesktop);
-  }
-  if (borderRadius) {
-    customStyles.borderRadius = getStyleArray(borderRadius, isDesktop);
-  }
-  if (spacing) {
-    customStyles.marginBottom = getStyle(spacing, isDesktop);
-  }
-  if (borderWidth) {
-    customStyles.borderWidth = convertUnit(borderWidth);
-  }
-  return /* @__PURE__ */ (0, import_jsx_runtime4.jsx)(
-    "div",
-    {
-      className: `
+        flex-8us4IE-${e}
+        flex-8us4IE-${`alignItems-${r}`}
+        flex-8us4IE-${`justifyContent-${n}`}
+        flex-8us4IE-${`flexDirection-${s}`}
+        ${o?"flex-8us4IE-wrap":""}
+        ${d?"flex-8us4IE-fullHeight":""}
+        ${p?"flex-8us4IE-fullWidth":""}
+        ${f?"flex-8us4IE-center":""}
+        ${h}
+      `,style:y,onClick:()=>{a&&a()},role:a?"button":"",children:t})},P=Y;var T=require("react/jsx-runtime"),Z=({children:t,backgroundColor:e="",padding:r,borderRadius:n,borderWidth:s,border:o="",opacity:i=100,shadow:u,overflow:d="visible",className:p="",spacing:h,onClick:l})=>{let f=g(1168),a={overflow:d};return r&&(a.padding=w(r,f)),n&&(a.borderRadius=w(n,f)),h&&(a.marginBottom=c(h,f)),s&&(a.borderWidth=m(s)),(0,T.jsx)("div",{className:`
         box-8us4IE 
-        ${backgroundColor ? `box-8us4IE-backgroundColor-${backgroundColor}-${opacity}` : ""}
-        ${border ? `box-8us4IE-border-${border}` : ""}
-        ${shadow ? `box-8us4IE-shadow-${shadow}` : ""}
-        ${className}
-      `,
-      style: customStyles,
-      onClick: () => {
-        onClick && onClick();
-      },
-      role: onClick ? "button" : "",
-      children
-    }
-  );
-};
-var Box_default = Box;
-
-// src/components/Text.tsx
-var import_jsx_runtime5 = require("react/jsx-runtime");
-var Text = ({
-  tagName,
-  mobile = false,
-  transform,
-  decoration,
-  fontWeight,
-  variant,
-  color,
-  className = "",
-  align,
-  spacing,
-  fontFamily,
-  children
-}) => {
-  const isDesktop = useMediaQuery(1168);
-  const Tag = tagName;
-  const customStyles = {};
-  if (spacing) {
-    customStyles.marginBottom = getStyle(spacing, isDesktop);
-  }
-  if (fontWeight) {
-    customStyles.fontWeight = fontWeight.toString();
-  }
-  return /* @__PURE__ */ (0, import_jsx_runtime5.jsx)(
-    Tag,
-    {
-      style: customStyles,
-      className: `
+        ${e?`box-8us4IE-backgroundColor-${e}-${i}`:""}
+        ${o?`box-8us4IE-border-${o}`:""}
+        ${u?`box-8us4IE-shadow-${u}`:""}
+        ${p}
+      `,style:a,onClick:()=>{l&&l()},role:l?"button":"",children:t})},F=Z;var M=require("react/jsx-runtime"),j=({tagName:t,mobile:e=!1,transform:r,decoration:n,fontWeight:s,variant:o,color:i,className:u="",align:d,spacing:p,fontFamily:h,children:l})=>{let f=g(1168),a=t,b={};return p&&(b.marginBottom=c(p,f)),s&&(b.fontWeight=s.toString()),(0,M.jsx)(a,{style:b,className:`
         text-8us4IE
-        text-8us4IE-variant-${variant}
-        text-8us4IE-fontFamily-${fontFamily}
-        ${color ? `text-8us4IE-color-${color}` : ""}
-        ${align ? `text-8us4IE-align-${align}` : ""}
-        ${mobile ? `text-8us4IE-mobile` : ""}
-        ${transform ? `text-8us4IE-transform-${transform}` : ""}
-        ${decoration ? `text-8us4IE-decoration-${decoration}` : ""}
-        ${className}
-      `,
-      children
-    }
-  );
-};
-var Text_default = Text;
-
-// src/components/Grid.tsx
-var import_jsx_runtime6 = require("react/jsx-runtime");
-var Grid = ({
-  children,
-  column,
-  gutter,
-  className = "",
-  spacing
-}) => {
-  const isDesktop = useMediaQuery(1168);
-  const customStyles = {};
-  if (spacing) {
-    customStyles.marginBottom = getStyle(spacing, isDesktop);
-  }
-  return /* @__PURE__ */ (0, import_jsx_runtime6.jsx)(
-    "div",
-    {
-      className: `
+        text-8us4IE-variant-${o}
+        text-8us4IE-fontFamily-${h}
+        ${i?`text-8us4IE-color-${i}`:""}
+        ${d?`text-8us4IE-align-${d}`:""}
+        ${e?"text-8us4IE-mobile":""}
+        ${r?`text-8us4IE-transform-${r}`:""}
+        ${n?`text-8us4IE-decoration-${n}`:""}
+        ${u}
+      `,children:l})},G=j;var L=require("react/jsx-runtime"),q=({children:t,column:e,gutter:r,className:n="",spacing:s})=>{let o=g(1168),i={};return s&&(i.marginBottom=c(s,o)),(0,L.jsx)("div",{className:`
         grid-8us4IE
-        grid-8us4IE-${getStyleSpacing(column, isDesktop)}-${getStyleSpacing(
-        gutter,
-        isDesktop
-      )}
-        ${className}
-      `,
-      style: customStyles,
-      children
-    }
-  );
-};
-var Grid_default = Grid;
-// Annotate the CommonJS export names for ESM import in node:
-0 && (module.exports = {
-  Box,
-  Button,
-  Flex,
-  Grid,
-  Text
-});
+        grid-8us4IE-${N(e,o)}-${N(r,o)}
+        ${n}
+      `,style:i,children:t})},W=q;0&&(module.exports={Box,Button,Flex,Grid,Text});
