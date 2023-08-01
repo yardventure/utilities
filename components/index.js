b8d2d7157a22675d7e086a09bc7481b62eec21bc
LAPTOP-3TEHD5AV\Work <gretavait@gmail.com>
2023-08-01 15:06:53 +0300

    content api + accordion added

--- components/index.js
+++ components/index.js
@@ -1,50 +1,50 @@
-"use strict";var S=Object.defineProperty;var O=Object.getOwnPropertyDescriptor;var V=Object.getOwnPropertyNames;var Y=Object.prototype.hasOwnProperty;var Z=(t,e)=>{for(var r in e)S(t,r,{get:e[r],enumerable:!0})},j=(t,e,r,n)=>{if(e&&typeof e=="object"||typeof e=="function")for(let o of V(e))!Y.call(t,o)&&o!==r&&S(t,o,{get:()=>e[o],enumerable:!(n=O(e,o))||n.enumerable});return t};var q=t=>j(S({},"__esModule",{value:!0}),t);var ut={};Z(ut,{Box:()=>T,Button:()=>D,Column:()=>X,Container:()=>v,Flex:()=>G,Grid:()=>Q,Row:()=>J,Text:()=>W});module.exports=q(ut);var P=require("react/jsx-runtime"),tt=()=>(0,P.jsx)("div",{className:`
+"use strict";var K=Object.create;var E=Object.defineProperty;var V=Object.getOwnPropertyDescriptor;var Y=Object.getOwnPropertyNames;var Z=Object.getPrototypeOf,ee=Object.prototype.hasOwnProperty;var te=(e,t)=>{for(var r in t)E(e,r,{get:t[r],enumerable:!0})},N=(e,t,r,n)=>{if(t&&typeof t=="object"||typeof t=="function")for(let o of Y(t))!ee.call(e,o)&&o!==r&&E(e,o,{get:()=>t[o],enumerable:!(n=V(t,o))||n.enumerable});return e};var re=(e,t,r)=>(r=e!=null?K(Z(e)):{},N(t||!e||!e.__esModule?E(r,"default",{value:e,enumerable:!0}):r,e)),ne=e=>N(E({},"__esModule",{value:!0}),e);var pe={};te(pe,{Box:()=>G,Button:()=>P,Column:()=>_,Container:()=>H,Flex:()=>A,Grid:()=>T,Row:()=>z,Text:()=>O});module.exports=ne(pe);var k=require("react/jsx-runtime"),oe=()=>(0,k.jsx)("div",{className:`
         loader-8us4IE
-      `}),R=tt;var w=(t,e)=>{if(e){if(t.lg)return t.lg.map(r=>m(r)).join(" ");if(t.sm)return t.sm.map(r=>m(r)).join(" ")}else return t.sm?t.sm.map(r=>m(r)).join(" "):"0"},f=(t,e)=>{if(e){if(t.lg==0)return"0";if(t.lg)return m(t.lg);if(t.sm)return m(t.sm)}else return t.sm?m(t.sm):"0"},B=(t,e)=>e?t.lg==0?t.lg:t.lg||t.sm:t.sm?t.sm:"0",k=(t,e)=>{if(e){if(t.lg)return t.lg[1]?t.lg.map(r=>r).join("-"):`${t.lg[0]}-${t.lg[0]}`;if(t.sm)return t.sm[1]?t.sm.map(r=>r).join("-"):`${t.sm[0]}-${t.sm[0]}`}else return t.sm?t.sm[1]?t.sm.map(r=>r).join("-"):`${t.sm[0]}-${t.sm[0]}`:"0"},m=t=>`${t/10}rem`;var b=require("react/jsx-runtime"),et=({as:t,buttonColor:e,textColor:r,href:n,external:o,variant:s="",buttonHeight:i,buttonWidth:u,borderRadius:d,shadow:$=!1,border:h,loading:l=!1,disabled:c=!1,startIcon:a,endIcon:p,className:x="",hint:C,transform:N,children:z,onClick:I})=>{let K=()=>{if(!c&&I){I();return}},y={};if(u&&(y.width=m(u)),i&&(y.height=m(i)),d&&(y.borderRadius=m(d)),n&&I)throw new Error("Please provider either href or onClick");return(0,b.jsx)("button",{style:y,className:`
+      `}),W=oe;var R=(e,t)=>{if(t){if(e.lg)return e.lg.map(r=>u(r)).join(" ");if(e.sm)return e.sm.map(r=>u(r)).join(" ")}else return e.sm?e.sm.map(r=>u(r)).join(" "):"0"},f=(e,t)=>{if(t){if(e.lg==0)return"0";if(e.lg)return u(e.lg);if(e.sm)return u(e.sm)}else return e.sm?u(e.sm):"0"},B=(e,t)=>t?e.lg==0?e.lg:e.lg||e.sm:e.sm?e.sm:"0",F=(e,t)=>{if(t){if(e.lg)return e.lg[1]?e.lg.map(r=>r).join("-"):`${e.lg[0]}-${e.lg[0]}`;if(e.sm)return e.sm[1]?e.sm.map(r=>r).join("-"):`${e.sm[0]}-${e.sm[0]}`}else return e.sm?e.sm[1]?e.sm.map(r=>r).join("-"):`${e.sm[0]}-${e.sm[0]}`:"0"},u=e=>`${e/10}rem`;var b=require("react/jsx-runtime"),se=({as:e,buttonColor:t,textColor:r,href:n,external:o,variant:s="",buttonHeight:i,buttonWidth:c,borderRadius:g,shadow:h=!1,border:y,loading:l=!1,disabled:m=!1,startIcon:a,endIcon:p,className:$="",hint:C,transform:w,children:X,onClick:S})=>{let q=()=>{if(!m&&S){S();return}},x={};if(c&&(x.width=u(c)),i&&(x.height=u(i)),g&&(x.borderRadius=u(g)),n&&S)throw new Error("Please provider either href or onClick");return(0,b.jsx)("button",{style:x,className:`
         button-8us4IE
-        button-8us4IE-buttonColor-${e}
+        button-8us4IE-buttonColor-${t}
         button-8us4IE-textColor-${r}
         ${s?`button-8us4IE-variant-${s}`:""}
-        ${h?"button-8us4IE-border":""}
-        ${$?"button-8us4IE-shadow":""}
+        ${y?"button-8us4IE-border":""}
+        ${h?"button-8us4IE-shadow":""}
         ${l?"button-8us4IE-loading":""}
-        ${N?`button-8us4IE-transform-${N}`:""}
-        ${x}
-      `,disabled:l||c,onClick:K,children:(0,b.jsx)(rt,{as:t,href:n,target:o?"_blank":"_self",children:(0,b.jsxs)("span",{className:"button-8us4IE-text",children:[a,z,p,l&&(0,b.jsx)(R,{}),C&&(0,b.jsx)("span",{className:"button-8us4IE-hint",children:C})]})})})},rt=({as:t,href:e,target:r,children:n})=>{let o=t;if(t&&!e||!t&&e)throw new Error("Please provide both - as and href props");return e?(0,b.jsx)(o,{href:e,target:r,className:"button-8us4IE-link",children:n}):(0,b.jsx)("div",{className:"button-8us4IE-link",children:n})},D=et;var E=require("react");function g(t){let[e,r]=(0,E.useState)(!1);return(0,E.useEffect)(()=>{let n=window.matchMedia(`(min-width: ${t}px)`);n.matches!==e&&r(n.matches);let o=()=>{r(n.matches)};return n.addEventListener("change",o),()=>n.removeEventListener("change",o)},[e,t]),e}var F=require("react/jsx-runtime"),nt=({children:t,display:e="flex",alignItems:r,justifyContent:n,flexDirection:o="row",wrap:s,columnGap:i,rowGap:u,fullHeight:d,fullWidth:$,className:h="",spacing:l,center:c,onClick:a})=>{let p=g(1168),x={};return i&&(x.columnGap=f(i,p)),u&&(x.rowGap=f(u,p)),l&&(x.marginBottom=f(l,p)),(0,F.jsx)("div",{className:`
+        ${w?`button-8us4IE-transform-${w}`:""}
+        ${$}
+      `,disabled:l||m,onClick:q,children:(0,b.jsx)(ie,{as:e,href:n,target:o?"_blank":"_self",children:(0,b.jsxs)("span",{className:"button-8us4IE-text",children:[a,X,p,l&&(0,b.jsx)(W,{}),C&&(0,b.jsx)("span",{className:"button-8us4IE-hint",children:C})]})})})},ie=({as:e,href:t,target:r,children:n})=>{let o=e;if(e&&!t||!e&&t)throw new Error("Please provide both - as and href props");return t?(0,b.jsx)(o,{href:t,target:r,className:"button-8us4IE-link",children:n}):(0,b.jsx)("div",{className:"button-8us4IE-link",children:n})},P=se;var I=require("react");function d(e){let[t,r]=(0,I.useState)(!1);return(0,I.useEffect)(()=>{let n=window.matchMedia(`(min-width: ${e}px)`);n.matches!==t&&r(n.matches);let o=()=>{r(n.matches)};return n.addEventListener("change",o),()=>n.removeEventListener("change",o)},[t,e]),t}var ae=re(require("axios"));var D=require("react");var M=require("react/jsx-runtime"),le=({children:e,display:t="flex",alignItems:r,justifyContent:n,flexDirection:o="row",wrap:s,columnGap:i,rowGap:c,fullHeight:g,fullWidth:h,className:y="",spacing:l,center:m,onClick:a})=>{let p=d(1168),$={};return i&&($.columnGap=f(i,p)),c&&($.rowGap=f(c,p)),l&&($.marginBottom=f(l,p)),(0,M.jsx)("div",{className:`
         flex-8us4IE
-        flex-8us4IE-${e}
+        flex-8us4IE-${t}
         flex-8us4IE-${`alignItems-${r}`}
         flex-8us4IE-${`justifyContent-${n}`}
         flex-8us4IE-${`flexDirection-${o}`}
         ${s?"flex-8us4IE-wrap":""}
-        ${d?"flex-8us4IE-fullHeight":""}
-        ${$?"flex-8us4IE-fullWidth":""}
-        ${c?"flex-8us4IE-center":""}
-        ${h}
-      `,style:x,onClick:()=>{a&&a()},role:a?"button":"",children:t})},G=nt;var M=require("react/jsx-runtime"),ot=({children:t,backgroundColor:e="",padding:r,borderRadius:n,borderWidth:o,border:s="",opacity:i=100,shadow:u,overflow:d="visible",className:$="",spacing:h,onClick:l})=>{let c=g(1168),a={overflow:d};return r&&(a.padding=w(r,c)),n&&(a.borderRadius=w(n,c)),h&&(a.marginBottom=f(h,c)),o&&(a.borderWidth=m(o)),(0,M.jsx)("div",{className:`
+        ${g?"flex-8us4IE-fullHeight":""}
+        ${h?"flex-8us4IE-fullWidth":""}
+        ${m?"flex-8us4IE-center":""}
+        ${y}
+      `,style:$,onClick:()=>{a&&a()},role:a?"button":"",children:e})},A=le;var v=require("react/jsx-runtime"),ue=({children:e,backgroundColor:t="",padding:r,borderRadius:n,borderWidth:o,border:s="",opacity:i=100,shadow:c,overflow:g="visible",className:h="",spacing:y,onClick:l})=>{let m=d(1168),a={overflow:g};return r&&(a.padding=R(r,m)),n&&(a.borderRadius=R(n,m)),y&&(a.marginBottom=f(y,m)),o&&(a.borderWidth=u(o)),(0,v.jsx)("div",{className:`
         box-8us4IE 
-        ${e?`box-8us4IE-backgroundColor-${e}-${i}`:""}
+        ${t?`box-8us4IE-backgroundColor-${t}-${i}`:""}
         ${s?`box-8us4IE-border-${s}`:""}
-        ${u?`box-8us4IE-shadow-${u}`:""}
-        ${$}
-      `,style:a,onClick:()=>{l&&l()},role:l?"button":"",children:t})},T=ot;var L=require("react/jsx-runtime"),st=({tagName:t,mobile:e=!1,transform:r,decoration:n,fontWeight:o,variant:s,color:i,className:u="",align:d,spacing:$,fontFamily:h,children:l})=>{let c=g(1168),a=t,p={};return $&&(p.marginBottom=f($,c)),o&&(p.fontWeight=o.toString()),(0,L.jsx)(a,{style:p,className:`
+        ${c?`box-8us4IE-shadow-${c}`:""}
+        ${h}
+      `,style:a,onClick:()=>{l&&l()},role:l?"button":"",children:e})},G=ue;var L=require("react/jsx-runtime"),ce=({tagName:e,mobile:t=!1,transform:r,decoration:n,fontWeight:o,variant:s,color:i,className:c="",align:g,spacing:h,fontFamily:y,children:l})=>{let m=d(1168),a=e,p={};return h&&(p.marginBottom=f(h,m)),o&&(p.fontWeight=o.toString()),(0,L.jsx)(a,{style:p,className:`
         text-8us4IE
         text-8us4IE-variant-${s}
-        text-8us4IE-fontFamily-${h}
+        text-8us4IE-fontFamily-${y}
         ${i?`text-8us4IE-color-${i}`:""}
-        ${d?`text-8us4IE-align-${d}`:""}
-        ${e?"text-8us4IE-mobile":""}
+        ${g?`text-8us4IE-align-${g}`:""}
+        ${t?"text-8us4IE-mobile":""}
         ${r?`text-8us4IE-transform-${r}`:""}
         ${n?`text-8us4IE-decoration-${n}`:""}
-        ${u}
-      `,children:l})},W=st;var A=require("react/jsx-runtime"),it=({children:t,column:e,gutter:r,className:n="",spacing:o})=>{let s=g(1168),i={};return o&&(i.marginBottom=f(o,s)),(0,A.jsx)("div",{className:`
+        ${c}
+      `,children:l})},O=ce;var j=require("react/jsx-runtime"),me=({children:e,column:t,gutter:r,className:n="",spacing:o})=>{let s=d(1168),i={};return o&&(i.marginBottom=f(o,s)),(0,j.jsx)("div",{className:`
         grid-8us4IE
-        grid-8us4IE-${B(e,s)}-${k(r,s)}
+        grid-8us4IE-${B(t,s)}-${F(r,s)}
         ${n}
-      `,style:i,children:t})},Q=it;var _=require("react/jsx-runtime"),at=({children:t})=>(0,_.jsx)("div",{className:"container-8us4IE",children:t}),v=at;var U=require("react/jsx-runtime"),lt=({children:t,padding:e,position:r,align:n})=>(0,U.jsx)("div",{className:`
+      `,style:i,children:e})},T=me;var Q=require("react/jsx-runtime"),fe=({children:e})=>(0,Q.jsx)("div",{className:"container-8us4IE",children:e}),H=fe;var J=require("react/jsx-runtime"),de=({children:e,padding:t,position:r,align:n})=>(0,J.jsx)("div",{className:`
     row-8us4IE
     ${r?`position-8us4IE-${r}`:""}
     ${n?`align-8us4IE-${n}`:""}
-  `,children:t}),J=lt;var H=require("react/jsx-runtime"),mt=({children:t,column:e,gutter:r})=>(0,H.jsx)("div",{className:`
-    col-8us4IE-${e}-${r}
-  `,children:t}),X=mt;0&&(module.exports={Box,Button,Column,Container,Flex,Grid,Row,Text});
+  `,children:e}),z=de;var U=require("react/jsx-runtime"),ge=({children:e,column:t,gutter:r})=>(0,U.jsx)("div",{className:`
+    col-8us4IE-${t}-${r}
+  `,children:e}),_=ge;0&&(module.exports={Box,Button,Column,Container,Flex,Grid,Row,Text});
--- functions/index.d.ts
+++ functions/index.d.ts
@@ -1,3 +1,38 @@
 declare function useMediaQuery(query: number): boolean;
 
-export { useMediaQuery };
+interface Read {
+    url: string;
+    params?: Object;
+}
+interface AuthRead extends Read {
+    token: string;
+}
+interface Write {
+    url: string;
+    params?: Object;
+    body?: Object;
+}
+interface AuthWrite extends Write {
+    token: string;
+}
+declare const publicFetch: ({ url, params }: Read) => Promise<any>;
+declare const publicModify: ({ url, params, body }: Write) => Promise<any>;
+declare const publicCreate: ({ url, params, body }: Write) => Promise<any>;
+declare const publicRemove: ({ url, body }: Write) => Promise<any>;
+declare const authFetch: ({ url, params, token }: AuthRead) => Promise<any>;
+declare const authCreate: ({ url, params, body, token }: AuthWrite) => Promise<any>;
+declare const authModify: ({ url, params, body, token }: AuthWrite) => Promise<any>;
+
+/**
+ * Custom hook which simplifies data fetching
+ * @param {Object} params - URL prameters that will be sent with the request
+ * @param {string} fetcher - Async function that accepts the params and returns the data
+ * @param {Array} [listeners=[]] - Array of dependencies which executes fetcher only if the dependencies have changed between renderings. By default it will run once
+ */
+declare const useFetch: (params: Object | false, fetcher: Function, listeners?: any[]) => {
+    data: any;
+    validating: boolean;
+    refresh: () => void;
+};
+
+export { authCreate, authFetch, authModify, publicCreate, publicFetch, publicModify, publicRemove, useFetch, useMediaQuery };
